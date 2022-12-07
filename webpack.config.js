133f18e01d87b9917137a7eda3e05181998c2f62
Rick Allen Magbanua <ramnashor2@gmail.com>
2022-12-07 20:24:31 +0800

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,40 @@
-const path = require("path");
+const path = require('path')
+
+const postCSSPlugins = {
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer")
+}
 
 module.exports = {
   entry: "./app/assets/scripts/App.js",
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "app"),
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app")
   },
   devServer: {
     before: function (app, server) {
-      server._watch("./app/**/*.html");
+      server._watch('./app/**/*.html')
     },
     contentBase: path.join(__dirname, "app"),
     hot: true,
     port: 3000,
-    host: "0.0.0.0",
+    host: '0.0.0.0'
   },
   mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-};
+        use: ["style-loader", "css-loader", {
+          loader: "postcss-loader", options: {
+            postcssOptions: { plugins: postCSSPlugins }
+          }
+        }]
+
+      }
+    ]
+  }
+}
